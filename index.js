382f5082214febdccfd590202f25b862df5a9408
localadmin <ghubl@allstate.com>
2018-06-11 16:19:31 +0530

    adding red and blue

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-document.body.innerHTML = "Hello world!";
+//document.body.innerHTML = "Hello world!";
+
+const Container= document.createElement('div');
+Container.className = 'MainDiv';
+
+document.body.appendChild(Container);
+
+for(let i=0;i<=10000;i++){
+    const col=document.createElement('div');
+    col.className='column';
+    col.addEventListener('click',()=>{
+        col.style.backgroundColor='red';
+    })
+    Container.appendChild(col);
+}
+
+const palette=document.createElement('div');
+palette.className='redcolorPalette';
+palette.addEventListener('click', ()=>{
+    palette.style.backgroundColor='red';
+})
+document.body.appendChild(palette);
+
+const palette2=document.createElement('div');
+palette2.className='bluecolorPalette';
+palette2.addEventListener('click', ()=>{
+    palette2.style.backgroundColor='blue';
+})
+document.body.appendChild(palette2);
