6efd95e976c579e45ad45db4f242f8dbe99067c8
localadmin <ghubl@allstate.com>
2018-06-12 09:43:19 +0530

    Adj kelsa

--- index.js
+++ index.js
@@ -2,28 +2,46 @@
 
 const Container= document.createElement('div');
 Container.className = 'MainDiv';
-
 document.body.appendChild(Container);
+let color='white';
 
 for(let i=0;i<=10000;i++){
     const col=document.createElement('div');
     col.className='column';
     col.addEventListener('click',()=>{
-        col.style.backgroundColor='red';
+        col.style.backgroundColor=color;
     })
     Container.appendChild(col);
 }
 
-const palette=document.createElement('div');
-palette.className='redcolorPalette';
-palette.addEventListener('click', ()=>{
-    palette.style.backgroundColor='red';
-})
-document.body.appendChild(palette);
+const colors = ['red', 'blue', 'green', 'brown', 'pink', 'grey', 'yellow', 'black','ghostwhite'];
+for (let col of colors){
+    const palette=document.createElement('div');
+    palette.className='coloredPalette';
+    palette.style.backgroundColor=col;
+    palette.addEventListener('click',()=>{
+        color=col;
+    })
+    Container.appendChild(palette);
+    }
 
-const palette2=document.createElement('div');
-palette2.className='bluecolorPalette';
-palette2.addEventListener('click', ()=>{
-    palette2.style.backgroundColor='blue';
-})
-document.body.appendChild(palette2);
+const curcol=document.createElement('div');
+curcol.className='currentColor';
+curcol.innerHTML='Current Color';
+curcol.style.backgroundColor=color;
+document.body.appendChild(curcol);
+
+// const palette=document.createElement('div');
+// palette.className='redcolorPalette';
+// palette.addEventListener('click', ()=>{
+//     color='red';
+// })
+// Container.appendChild(palette);
+//
+// const palette2=document.createElement('div');
+// palette2.className='bluecolorPalette';
+// palette2.addEventListener('click', ()=>{
+//     color='blue';
+// })
+// Container.appendChild(palette2);
+//
